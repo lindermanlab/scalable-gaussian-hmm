a1b7873fb8df8a725f6c9e6e7b2774677b653d13
Libby Zhang <ezhang94@github.com>
2022-06-14 11:22:49 -0700

    removed old sharded e step code

--- src/kf/inference.py
+++ src/kf/inference.py
@@ -114,68 +114,6 @@ def collective_m_step(nss: NormalizedGaussianHMMSuffStats) -> GaussianHMM:
                        emission_means,
                        emission_covs)
 
-def partial_e_step(hmm: GaussianHMM, emissions: np.ndarray) -> NormalizedGaussianHMMSuffStats:
-    """Computes posterior expected sufficient stats given partial batch of emissions.
-
-    Inputs
-        hmm: GaussianHMM with K states
-        emissions: shape (T_m, D)
-
-    Returns
-        NormalizedGaussianHMMSuffStats dataclass
-    """
-
-    # Compute posterior HMM distribution
-    lps = hmm.emission_distribution.log_prob(emissions[..., None, :])
-    posterior =  \
-            hmm_smoother(hmm.initial_probabilities, hmm.transition_matrix, lps)
-
-    # Compute expected sufficient statistics
-    w_normalizer = posterior.smoothed_probs.sum(axis=0)                         # shape (K,). sum_i w_i, shape (K,)
-    normd_weights = posterior.smoothed_probs / w_normalizer                                  # shape (T,K)
-
-    normd_Ex = np.einsum('tk, ti->ki', normd_weights, emissions)
-    normd_ExxT = np.einsum('tk, ti, tj->kij',
-                              normd_weights, emissions, emissions)
-
-    initial_probs = posterior.smoothed_probs[0]
-    transition_probs = posterior.smoothed_transition_probs.sum(axis=0)
-
-    return initial_probs, transition_probs, w_normalizer, normd_Ex, normd_ExxT
-
-def m_step(initial_state_probs, transition_counts, ws, weighted_Ex, weighted_ExxT):
-    """Compute ML parameters from batched expected sufficient statistics.
-
-    Inputs:
-        initial_state_probs: shape (M,K)
-        transition_counts: shape (M,K,K)
-        ws: shape (M,K)
-        w_Exs: shape (M,K,D)
-        w_ExxTs: shape (M,K,D,D)
-    Returns:
-        hmm: GaussianHMM
-    """
-
-    # Initial distribution
-    initial_state_prob = Dirichlet(1.0001 + initial_state_probs[0]).mode()
-
-    # Transition distribution
-    transition_matrix = Dirichlet(1.0001 + transition_counts.sum(0)).mode()
-
-    # Gaussian emission distribution
-    emission_dim = weighted_Ex.shape[-1]
-    weights = ws / ws.sum(axis=0)                                               # shape (M,K,)
-    emission_means = (weighted_Ex * weights[:,:,None]).sum(axis=0)
-    emission_covs  = (weighted_ExxT * weights[:,:,None,None]).sum(axis=0) \
-                     - np.einsum('ki,kj->kij', emission_means, emission_means) \
-                     + 1e-4 * np.eye(emission_dim)
-
-    # Pack the results into a new GaussianHMM
-    return GaussianHMM(initial_state_prob,
-                       transition_matrix,
-                       emission_means,
-                       emission_covs)
-
 # ==============================================================================
 # "Standard" full-batch EM steps
 #
