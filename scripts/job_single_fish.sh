98b0bbc4899ec2d5e244cdb7f72d3b55c6e10ab8
Libby Zhang <ezhang94@github.com>
2022-06-27 14:56:47 -0700

    fixed sbatch issues
    lol, it has to be `#SBATCH`, aka no space

--- scripts/job_single_fish.sh
+++ scripts/job_single_fish.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-# SBATCH -c=8
-# SBATCH --time=01:00:00
-# SBATCH --mem=8G
-# SBATCH -p=hns
-# SBATCH --job-name=single_fish_pmap
-# SBATCH --output=/scratch/users/%u/kf_tmp/job.%j.out
-# SBATCH --error=/scratch/users/%u/kf_tmp/job.%j.err
+#SBATCH --job-name=single_fish_pmap
+#SBATCH --partition=hns
+#SBATCH --cpus-per-task=4
+#SBATCH --mincpus=4
+#SBATCH --time=01:00:00
+#SBATCH --mem=8G
+#SBATCH --chdir ~/killifish/scripts/
+#SBATCH --output=/scratch/users/%u/kf_tmp/job.%j.out
+#SBATCH --error=/scratch/users/%u/kf_tmp/job.%j.err
+
+# NB: Important to specify `mincpus`` to be <= `cpus-per-task``.
+# This is the minimum number of CPUs required on node, and a "hard-requirement"
+# for SLURM to fulfill, where as `cpus-per-task` is more a "soft-requirement". 
+# If unspecified, Slurm happily allocates a node with 1 CPU.
 
 # ================================================
 # Slurm job script for testing EM on a single fish
-# Loading partial day of data
 # ================================================
 
 # Load modules and activate virtual environment
@@ -18,22 +24,15 @@ source ~/.bashrc
 
 module purge > /dev/null
 module load python/3.9.0
-module list
 
 venv activate kf-cpu
-echo PATH
-echo $PATH
 
-# Manually set XLA_FLAGS to number of CPUs requested
-echo CPUS_ON_NODE $SLURM_CPUS_ON_NODE $SLURM_CPUS_PER_TASK
-# export XLA_FLAGS=--xla_force_host_platform_device_count=$SLURM_CPUS_ON_NODE
-export XLA_FLAGS=--xla_force_host_platform_device_count=8
-echo XLA_FLAGS $XLA_FLAGS
+# Set XLA_FLAGS to match number of CPUs allocated for this job
+echo 'CPUS for this job ' $SLURM_JOB_CPUS_PER_NODE, 'CPUS available on this node ' $SLURM_CPUS_ON_NODE
+export XLA_FLAGS=--xla_force_host_platform_device_count=$SLURM_JOB_CPUS_PER_NODE
 
+# ---------------------------------------------------------------------------
 # HMM FIT PARAMETERS
-cd ~/killifish/scripts/
-
-seed=20220627
 # Memory req's determined by size of these 2 parameters: how much data is loaded
 # per partial-EM iteration, and as a result, how much compute memory req'd
 # 10 x 8h of data [f/s x s/min x min/hr] ~= 22 MB
@@ -53,7 +52,7 @@ frac_test=0.005  # 1 file
 num_hmm_states=20
 num_em_iters=20
 
-# TODO IF I don't pipe python to indicated file, does it pipe to slurm output?
-# python3 -u script_pmap_single.py --method pmap --seed $seed --batch_size $batch_size --num_frames_per_batch $num_frames_per_batch --num_train $frac_train --num_test $frac_test --num_hmm_states $num_hmm_states --num_em_iters $num_em_iters > "$SCRATCH/kf_tmp/$SLURM_JOB_ID-stdout.out"
+seed=20220627
 
-python3 -u script_pmap_single.py --method pmap --seed $seed --batch_size $batch_size --num_frames_per_batch $num_frames_per_batch --num_train $frac_train --num_test $frac_test --num_hmm_states $num_hmm_states --num_em_iters $num_em_iters
+# python -u flag: Force  stdin,  stdout and stderr to be totally unbuffered.
+python -u script_pmap_single.py --method pmap --seed $seed --batch_size $batch_size --num_frames_per_batch $num_frames_per_batch --num_train $frac_train --num_test $frac_test --num_hmm_states $num_hmm_states --num_em_iters $num_em_iters
