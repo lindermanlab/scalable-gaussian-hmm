4c1432c402506251c85baebb34ec2a1483282627
Libby <elizabeth.zhang94@gmail.com>
2022-12-08 14:52:29 -0800

    removed erroneous scale by normalizer in posterior transition concentration

--- src/kf/gaussian_hmm/_algorithms.py
+++ src/kf/gaussian_hmm/_algorithms.py
@@ -125,7 +125,7 @@ def m_step(prior_params, normalized_stats, normalizer):
 
     # Calculate mode of posterior transition distribution (Dirichlet)
     posterior_transition_conc = (
-        normalized_stats.transition_pseudocounts * normalizer\
+        normalized_stats.transition_pseudocounts \
         + jnp.nan_to_num(prior_params.transition_probs_conc / normalizer, nan=0.0)
     )
     # transition_probs = Dirichlet(posterior_transition_conc).mode()
